19398f28efa357944249973664779e3b575e51f1
ciel <cielchang0324@gmail.com>
2023-10-02 17:58:04 +0800

    新增layout的ejs 更新tailwind.config.js內的顏色，container，字體設定 引入google font的字體，並且在main.css中設定完成 修改font-sans的字體，新增google font的 "Noto Sans TC" 修改修改font-serif的字體，新增google font的 Shrikhand

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: ['./pages/**/*.html', './layout/**/*.ejs', './main.js'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '12px'
+    },
+    fontFamily:{
+      'sans':['"Noto Sans TC"',...defaultTheme.fontFamily.sans],
+      'serif':['Shrikhand',...defaultTheme.fontFamily.serif]
+    },
+    extend: {
+      colors:{
+        'primary':{
+          DEFAULT:'#7000FF',
+          'light':'#7000FF',
+          'dark':'#11002D',
+          '100':'#6E01F8',
+          '200':'#380C7E',
+          '300':'#1F0F39'
+        },
+        'neutral':{
+          DEFAULT: '#E9E2F3',
+          'light':'#28085C',
+          'dark':'#000000',
+          'white':'#ffffff',
+        }
+      }
+    },
   },
   plugins: [],
 }
